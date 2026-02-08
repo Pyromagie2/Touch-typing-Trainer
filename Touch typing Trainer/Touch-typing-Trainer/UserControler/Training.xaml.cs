763a13f6527d5632797921d7832f71ac87e59294
Pyromagie2 <160120693+Pyromagie2@users.noreply.github.com>
2026-02-08 16:21:07 +0100

    now you can type and when its the right key its turns green when wrong it turns red and you have a pointer to help you to know where your position is

--- Touch typing Trainer/Touch-typing-Trainer/UserControler/Training.xaml.cs
+++ Touch typing Trainer/Touch-typing-Trainer/UserControler/Training.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
 using Microsoft.Win32;
 
 
@@ -13,6 +15,7 @@ namespace Touch_typing_Trainer
         int posisiton = 0;
         private char inputKey;
         
+        
         public Training()
         {
             InitializeComponent();
@@ -20,6 +23,7 @@ namespace Touch_typing_Trainer
             this.TextInput += TrainingInput;
             this.PreviewKeyDown += Training_PreviewKeyDown;
             textToList();
+            setTextPointer();
         }
         
         private void Training_Loaded(object sender, RoutedEventArgs e)
@@ -32,50 +36,69 @@ namespace Touch_typing_Trainer
             if (e.Key == Key.Back && posisiton > 0)
             {
                 posisiton--;
+                var runTemp = trainingText.Inlines.ElementAt(posisiton) as Run;
+                runTemp.Background = Brushes.White;
+                runTemp.Foreground = Brushes.Black;
                 e.Handled = true;
             }
             
         }
 
+        public void setTextPointer()
+        {
+            var run = trainingText.Inlines.ElementAt(posisiton) as Run;
+            run.Background = Brushes.BurlyWood;
+        }
         
   
         
         public void textToList()
         {
             
-            string path = string.Empty;
+            textList.Clear();
+            trainingText.Inlines.Clear();
+            posisiton = 0;
             
-            OpenFileDialog ofd = new OpenFileDialog();
+           
 
-            ofd.Filter = "(*.txt)|*.txt";
-            ofd.FilterIndex = 1;
-            ofd.Title = "Choose a txt File";
+            
+            string path = string.Empty;
+
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "(*.txt)|*.txt",
+                FilterIndex = 1,
+                Title = "Choose a txt File"
+            };
 
             if (ofd.ShowDialog() == true)
             {
                 path = ofd.FileName;
             }
-
+            
             if (string.IsNullOrEmpty(path))
             {
                 MessageBox.Show("Please select a file");
                 textToList();
                 return;
             }
-            
+
             string text = File.ReadAllText(path);
-            
-            trainingText.Text = text;
-            
+
             for (int i = 0; i < text.Length; i++)
             {
-                textList.Add(text[i]);
-            }
-            foreach (char c in textList)
-            {
+                char c = text[i];
+                textList.Add(c);
                 Console.Write(c);
+                var run = new Run(c.ToString())
+                {
+                    Foreground = Brushes.Black 
+                };
+                trainingText.Inlines.Add(run);
+                
             }
         }
+
 
         
         private void TrainingInput(object sender, TextCompositionEventArgs e)
@@ -88,7 +111,7 @@ namespace Touch_typing_Trainer
             }
 
             TypingExercise();
-            
+            setTextPointer();
             
             
             e.Handled = true;
@@ -104,14 +127,26 @@ namespace Touch_typing_Trainer
 
         public void TypingExercise()
         {
+       
+
+            char currentCharacter = textList[posisiton];
             
-                char currentCharacter = textList[posisiton];
-                if (currentCharacter == inputKey)
-                { 
-                  Console.Write("GAAAAAAA");
-                }
-                
-            
+            var run = trainingText.Inlines.ElementAt(posisiton) as Run;
+
+            if (run == null)
+                return;
+
+            if (currentCharacter == inputKey)
+            {
+                run.Foreground = Brushes.Green;  
+            }
+            else
+            {
+                run.Foreground = Brushes.Red;    
+            }
+
+            posisiton++;
         }
+
     }
 }
